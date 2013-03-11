62785fff7bc87dbab6e6a9775eef3f44bc131c0a
zythum <zythum02@gmail.com>
2013-03-11 11:05:41 +0800

    ror的练习
    install rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0d8921e1ee4f1a33abe09169edec9e0c874624b35a0af44e19ed009877d19763478a6b04b07ca4bc90167c0e60d8c7d037758a368c9d23bf97765cdc8f786256'
